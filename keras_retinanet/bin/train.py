6be81577e6761c842b0a0e7797afbf49c20b2167
Paul Kreymborg <frenebo@gmail.com>
2019-05-16 17:55:55 -0700

    Removed unnecessary indexing, again

--- keras_retinanet/bin/train.py
+++ keras_retinanet/bin/train.py
@@ -552,7 +552,7 @@ def main(args=None):
         outputs=[training_model.layers[25].output, training_model.layers[150].output])
     for i in range(train_generator.__len__()):
         in_val = train_generator.__getitem__(i)[0]
-        pred25, pred150 = intermediate_model.predict(in_val)[0]
+        pred25, pred150 = intermediate_model.predict(in_val)
         np.save("kr_middle_layers/pred25", pred25)
         np.save("kr_middle_layers/pred150", pred150)
 
