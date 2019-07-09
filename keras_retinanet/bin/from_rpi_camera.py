2fddba256fdb115adea253a23693cafcb47ee635
Paul Kreymborg <frenebo@gmail.com>
2019-07-09 13:37:20 -0700

    added lines

--- keras_retinanet/bin/from_rpi_camera.py
+++ keras_retinanet/bin/from_rpi_camera.py
@@ -24,14 +24,22 @@ def gstreamer_pipeline(
     framerate=15,
     flip_method=0):
 
-    return ('nvarguscamerasrc ! '
-    'video/x-raw(memory:NVMM), '
-    'width=(int)%d, height=(int)%d, '
-    'format=(string)NV12, framerate=(fraction)%d/1 ! '
-    'nvvidconv flip-method=%d ! '
-    'video/x-raw, width=(int)%d, height=(int)%d, format=(string)BGRx ! '
-    'videoconvert ! '
-    'video/x-raw, format=(string)BGR ! appsink'  % (capture_width,capture_height,framerate,flip_method,display_width,display_height))
+    return (
+        'nvarguscamerasrc ! '
+        'video/x-raw(memory:NVMM), '
+        'width=(int)%d, height=(int)%d, '
+        'format=(string)NV12, framerate=(fraction)%d/1 ! '
+        'nvvidconv flip-method=%d ! '
+        'video/x-raw, width=(int)%d, height=(int)%d, format=(string)BGRx ! '
+        'videoconvert ! '
+        'video/x-raw, format=(string)BGR ! appsink' % (
+            capture_width,
+            capture_height,
+            framerate,
+            flip_method,
+            display_width,display_height
+        )
+    )
 
 
 def load_graph_def(frozen_graph_filename):
