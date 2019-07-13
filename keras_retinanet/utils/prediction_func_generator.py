d345e59a767dbb6a2b7a1362dadbd98d53109dff
Paul Kreymborg <frenebo@gmail.com>
2019-07-13 13:25:43 -0700

    Added prints, made image resize smaller

--- keras_retinanet/utils/prediction_func_generator.py
+++ keras_retinanet/utils/prediction_func_generator.py
@@ -38,36 +38,49 @@ def generate_prediction_func(
         score_threshold=0.05, # threshold score for showing prediction
     ):
 
+    print("Loading graph definition... ", end="")
     graph_def = load_graph_def(frozen_graph_filename)
+    print("Done loading graph definition")
 
+    print("Creating TF session... ", end="")
     tf_config = tf.ConfigProto()
     tf_config.gpu_options.allow_growth = True
     tf_sess = tf.Session(config=tf_config)
+    print("Done creating TF session")
+
+    print("Importing graph definition... ", end="")
     tf.import_graph_def(graph_def, name='')
+    print("Done importing graph_definition ", end="")
 
     preprocess_image = models.backbone(backbone_name).preprocess_image
     label_to_name = csv_label_to_name_func(csv_classes_path)
 
+    print("Getting graph output tensors... ", end="")
     boxes_tensor = tf_sess.graph.get_tensor_by_name("ident_boxes/Identity:0")
     scores_tensor = tf_sess.graph.get_tensor_by_name("ident_scores/Identity:0")
     labels_tensor = tf_sess.graph.get_tensor_by_name("ident_labels/Identity:0")
-
+    print("Done getting graph output tensors")
 
 
     def pred_func(raw_image):
+        print("Preprocessing image... ", end="")
         image        = preprocess_image(raw_image.copy())
-        image, scale = resize_image(image)
+        image, scale = resize_image(image, min_side=300, max_side=500)
         image = np.expand_dims(image, axis=0)
+        print("Done preprocessing image")
 
         feed_dict = {
             "input_1:0": image
         }
 
+        print("Running TF session on image... ", end="")
         boxes, scores, labels = tf_sess.run([boxes_tensor, scores_tensor, labels_tensor], feed_dict)
+        print("Done running tf session on image")
         # print("boxes: ", boxes.shape)
         # print("scores: ", scores.shape)
         # print("labels: ", labels.shape)
 
+        print("Extracting predictions from session output... ", end="")
         boxes /= scale
         indices = np.where(scores[0, :] > score_threshold)[0]
 
@@ -77,15 +90,17 @@ def generate_prediction_func(
         image_boxes      = boxes[0, indices[scores_sort], :]
         image_scores     = scores[scores_sort]
         image_labels     = labels[0, indices[scores_sort]]
+        print("Done extracting predictions")
 
+        print("Drawing detections... ", end="")
         ret_img = raw_image.copy()
-
         draw_detections(
             ret_img,
             image_boxes,
             image_scores,
             image_labels,
             label_to_name=label_to_name)
+        print("Done drawing detections")
 
         return ret_img
 
