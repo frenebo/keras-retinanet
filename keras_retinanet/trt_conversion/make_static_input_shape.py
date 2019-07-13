e023cc1e070beba807319dcd71f7afb3d840c72f
Paul Kreymborg <frenebo@gmail.com>
2019-07-13 15:30:01 -0700

    added input shape conversion script

---
+++ keras_retinanet/trt_conversion/make_static_input_shape.py
@@ -0,0 +1,34 @@
+import importlib
+from keras import backend as K
+import keras
+# This line must be executed before loading Keras model.
+K.set_learning_phase(0)
+
+import tensorflow as tf
+
+
+# Allow relative imports when being executed as script.
+if __name__ == "__main__" and __package__ is None:
+    sys.path.insert(0, os.path.join(os.path.dirname(__file__), '..', '..'))
+    import keras_retinanet.trt_conversion  # noqa: F401
+    __package__ = "keras_retinanet.trt_conversion"
+
+from .. import models
+
+def main():
+    parser = argparse.ArgumentParser(description='Make model have static input shape 1,224,224,3')
+    parser.add_argument("source_model", help="Source model path")
+    parser.add_argument("static_model_save", help="Path to save static model")
+
+    args = parser.parse_args()
+
+    model = models.load_model(args.source_model)
+
+    new_input = keras.layers.Input(batch_input_shape=(1, 224, 224, 3))
+
+    model.layers[0] = new_input
+
+    model.save(args.static_model_save)
+
+if __name__ == "__main__":
+    main()
